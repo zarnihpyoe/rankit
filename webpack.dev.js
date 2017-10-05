93f5c7ec609e377d4702a9c37481f8ab2cebc6fc
Zarni Hpyoe <zarnihpyoe@gmail.com>
2017-10-05 14:33:28 -0400

    setup: Setup webpack and dev dependencies

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+const merge = require('webpack-merge');
+
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map', // source map for easier debug (don't do this for production)
+  devServer: {
+    // telling webpack-dev-server
+    contentBase: './dist' // - to serve files from ./dist on localhost:8080
+  }
+});
---
+++ webpack.prod.js
@@ -0,0 +1,8 @@
+const merge = require('webpack-merge');
+
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'source-map'
+  // webpack -p = webpack --optimize-minize --define process.env.NODE_ENV="'production'"
+});
